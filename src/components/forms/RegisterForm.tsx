0dcf0e13b3b5f07237a3ee24f605d8d4313b3f83
Informatica <informatica@iMac-de-Informatica.local>
2023-03-06 16:59:41 -0600

    V11 progress

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,1 +1,30 @@
 import React from "react";
+
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+
+import { register } from "../../services/authService";
+import { AxiosResponse } from "axios";
+
+
+const registerForm = () => {
+    const initialValues = {
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        age: 18
+    }
+
+    //Yup schema validator
+    const registerSchema = Yup.object().shape({
+        name: Yup.string().min(6, 'Username must have 6 letters minimum').max(14, 'User name must have 14 latter max').required('Username is required'),
+        email: Yup.string().email('Invalid email format').required('Email is required'),
+        password: Yup.string().min(6, 'Password too short (6)').required('Password is required'),
+        confirm: Yup.string().when("password", {
+             is: (value: string) => (value && value.length > 0 ? true: false),
+             
+            })
+    })
+
+}
